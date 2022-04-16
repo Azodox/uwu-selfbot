25499965b85ae745349d2476d4d77aa925d02853
Azodox_ <likemoi99@gmail.com>
2022-04-16 16:26:50 +0200

    Corrected some errors and added logs.

--- check.py
+++ check.py
@@ -38,49 +38,49 @@ short_names_to_delivery_prices = {
     "lebonmatin": 95
 }
 
-def check_price(displayed_price, s):
+
+async def check_price(displayed_price: int, s: str):
     delivery = False
     discount = False
 
     total = 0
     prime = 0
 
+    brackets_options = re.findall(r'\[(.*?)\]', s)
+    if brackets_options is not None:
+        for option in brackets_options:
+            if option == "Livraison":
+                delivery = True
+            elif option == "Réduction 10%":
+                discount = True
+    tip = 0
+    if "(" in s:
+        tip = re.search(r".*?\((.*?)\)", s).group(1)
+        tip = tip.replace("$", "").replace(" pb", "")
+
+    if discount is True or delivery is True:
+        s = s.split("]")[2 if discount is True and delivery is True else 1]
+
+    regex = r"(.*?){}".format(r"\(" if tip != 0 else "")
+    content = re.search(regex, s).group(1) if regex != "(.*?)" else s
+    split = re.split(r"([0-9]+)([a-z]+)", content.lower().replace(" ", ""))
+
     try:
-        bracketsOptions = re.findall(r'\[(.*?)\]', s)
-        if bracketsOptions is not None:
-            for option in bracketsOptions:
-                if option == "Livraison":
-                    delivery = True
-                elif option == "Réduction 10%":
-                    discount = True
-        tip = 0
-        if "(" in s:
-            tip = re.search(r".*?\((.*?)\)", s).group(1)
-            tip = tip.replace("$", "").replace(" pb", "")
-
-        if discount is True or delivery is True:
-            s = s.split("]")[2 if discount is True and delivery is True else 1]
-
-        regex = r"(.*?){}".format(r"\(" if tip != 0 else "")
-        content = re.search(regex, s).group(1) if regex != "(.*?)" else s
-        split = re.split(r"([0-9]+)([a-z]+)", content.lower().replace(" ", ""))
-
         for i in range(0, len(split)):
             item = split[i]
 
             if item != "" and item.isnumeric() is False:
-                price = int(split[i - 1]) * int(
-                    short_names_to_normal_prices.get(item) if delivery is False else short_names_to_delivery_prices.get(
-                        item))
-                total += price
-                prime += 5
+                amount = int(split[i - 1])
+                total_price = amount * int(short_names_to_normal_prices.get(item) if delivery is False else int(short_names_to_delivery_prices.get(item)))
+                total += total_price
+                prime += 5 * amount
     except:
+        print("Found a bill with invalid declaration, skipping...")
         return {"value": False, "prime": 0}
-
 
     if int(displayed_price) == (int(total + int(tip)) if discount is False else int(int(total + int(tip)) * 0.9)):
         if tip != 0:
-            prime += int(tip * 0.75)
+            prime += int(int(tip) * 0.75)
         return {"value": True, "prime": prime}
     else:
         return {"value": False, "prime": 0}
