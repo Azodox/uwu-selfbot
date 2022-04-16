def0831be80d3fc1d4077e5eea08ef1feecca600
Azodox_ <likemoi99@gmail.com>
2022-04-16 16:46:40 +0200

    Added logging.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import check
 import json
 import discord
+import logging
 
 
 with open('config.json') as config_file:
     options = json.load(config_file)
+
+    if options['token'] == "" or options['channelID'] == "" or options['guildID'] == "":
+        logging.error("Invalid config.json")
+        exit(1)
 
 
 async def main():
@@ -18,8 +23,8 @@ async def main():
             channel = ch
 
     if channel is None:
-        print("Channel not found")
-        return
+        logging.error("Channel not found")
+        exit(1)
 
     skipped_bills_amount = 0
     if input("Voulez-vous vérifier une facture en particulier ? (o/n) ") == "o":
@@ -47,20 +52,20 @@ async def main():
                     description = fields[1]
 
             if id == "" or author == "" or price == "" or description == "":
-                print("The bill is not well formatted, skipping...")
+                logging.debug("The bill is not well formatted, skipping...")
                 break
 
-            print("Bill found : " + id)
+            logging.debug("Bill found : " + id)
             result = await check.check_price(int(price), description)
 
             if result['value'] is True:
-                print("Bill " + id + " is valid.")
+                logging.debug("Bill " + id + " is valid.")
                 if bonus.get(author) is None:
                     bonus.__setitem__(author, result['prime'])
                 else:
                     bonus[author] += result['prime']
             else:
-                print("Bill " + id + " is not valid.")
+                logging.debug("Bill " + id + " is not valid.")
                 skipped_bills_amount += 1
     else:
         end = input("Entrez l'id de la première facture : ")
@@ -71,7 +76,7 @@ async def main():
         async for message in channel.history(limit=int(input("Combien de messages de facture avez-vous besoin de récupérer ? "))):
             for embed in message.embeds:
                 if embed.title != "Facture payée":
-                    print("Found a non-paid bill, skipping...")
+                    logging.debug("Found a non-paid bill, skipping...")
                     break
 
                 id = ""
@@ -90,20 +95,20 @@ async def main():
                         description = fields[1]
 
                 if id == "" or author == "" or price == "" or description == "":
-                    print("Found a bill with missing fields, skipping...")
+                    logging.debug("Found a bill with missing fields, skipping...")
                     break
 
                 if id == start:
-                    print("Found the first bill.")
+                    logging.debug("Found the first bill.")
                     bills.append({"id": id, "author": author, "price": price, "description": description})
                     automatically_add = True
 
                 if automatically_add is True:
-                    print("Adding bill " + id + " to the list.")
+                    logging.debug("Adding bill " + id + " to the list.")
                     bills.append({"id": id, "author": author, "price": price, "description": description})
 
                 if id == end:
-                    print("Found the last bill.")
+                    logging.debug("Found the last bill.")
                     bills.append({"id": id, "author": author, "price": price, "description": description})
                     automatically_add = False
 
@@ -115,17 +120,18 @@ async def main():
 
             result = await check.check_price(price, description)
             if result['value'] is True:
-                print("Bill " + id + " is valid.")
+                logging.debug("Bill " + id + " is valid.")
                 if bonus.get(author) is None:
                     bonus.__setitem__(author, result['prime'])
                 else:
                     bonus[author] += result['prime']
             else:
-                print("Bill " + id + " is not valid.")
+                logging.debug("Bill " + id + " is not valid.")
                 skipped_bills_amount += 1
 
-    print("Factures non valides : " + str(skipped_bills_amount))
-    print("Primes : " + str(bonus))
+    logging.log("Factures non valides : " + str(skipped_bills_amount))
+    logging.log("Primes : " + str(bonus))
+    exit()
 
 
 class Client(discord.Client):
