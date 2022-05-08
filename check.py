932480bb772259eb9d944e6ec08fe7c4648d9477
Azodox_ <likemoi99@gmail.com>
2022-05-08 02:31:16 +0200

    Updated prices.

--- check.py
+++ check.py
@@ -1,19 +1,19 @@
 import re
 
 short_names_to_normal_prices = {
-    "cg": 50,
-    "cgv": 65,
-    "cgc": 65,
-    "c": 40,
-    "cd": 40,
-    "ca": 50,
-    "cl": 45,
-    "a": 45,
-    "m": 50,
-    "cc": 55,
-    "ch": 45,
-    "cr": 35,
-    "mf": 40,
+    "cg": 70,
+    "cgv": 85,
+    "cgc": 85,
+    "c": 60,
+    "cd": 60,
+    "ca": 70,
+    "cl": 65,
+    "a": 65,
+    "m": 70,
+    "cc": 75,
+    "ch": 65,
+    "cr": 55,
+    "mf": 60,
     "gfingsoif": 135,
     "chocochoco": 100,
     "lebonmatin": 75,
@@ -21,23 +21,23 @@ short_names_to_normal_prices = {
 }
 
 short_names_to_delivery_prices = {
-    "cg": 60,
-    "cgv": 75,
-    "cgc": 75,
-    "c": 50,
-    "cd": 50,
-    "ca": 60,
-    "cl": 55,
-    "a": 55,
-    "m": 60,
-    "cc": 65,
-    "ch": 55,
-    "cr": 45,
-    "mf": 50,
-    "gfingsoif": 150,
-    "chocochoco": 120,
-    "lebonmatin": 95,
-    "mac": 120
+    "cg": 80,
+    "cgv": 95,
+    "cgc": 95,
+    "c": 70,
+    "cd": 70,
+    "ca": 80,
+    "cl": 75,
+    "a": 75,
+    "m": 80,
+    "cc": 85,
+    "ch": 75,
+    "cr": 65,
+    "mf": 70,
+    "gfingsoif": 180,
+    "chocochoco": 160,
+    "lebonmatin": 135,
+    "mac": 110
 }
 
 
@@ -55,19 +55,20 @@ async def check_price(displayed_price: int, s: str):
                 delivery = True
             elif option == "Réduction 10%":
                 discount = True
-    tip = 0
-    if "(" in s:
-        tip = re.search(r".*?\((.*?)\)", s).group(1)
-        tip = tip.replace("$", "").replace(" pb", "")
-
-    if discount is True or delivery is True:
-        s = s.split("]")[2 if discount is True and delivery is True else 1]
-
-    regex = r"(.*?){}".format(r"\(" if tip != 0 else "")
-    content = re.search(regex, s).group(1) if regex != "(.*?)" else s
-    split = re.split(r"([0-9]+)([a-z]+)", content.lower().replace(" ", ""))
 
     try:
+        tip = 0
+        if "(" in s:
+            tip = re.search(r".*?\((.*?)\)", s).group(1)
+            tip = tip.replace("$", "").replace(" pb", "")
+
+        if discount is True or delivery is True:
+            s = s.split("]")[2 if discount is True and delivery is True else 1]
+
+        regex = r"(.*?){}".format(r"\(" if tip != 0 else "")
+        content = re.search(regex, s).group(1) if regex != "(.*?)" else s
+        split = re.split(r"([0-9]+)([a-z]+)", content.lower().replace(" ", ""))
+
         for i in range(0, len(split)):
             item = split[i]
 
@@ -76,13 +77,13 @@ async def check_price(displayed_price: int, s: str):
                 total_price = amount * int(short_names_to_normal_prices.get(item) if delivery is False else int(short_names_to_delivery_prices.get(item)))
                 total += total_price
                 prime += 5 * amount
+
+        if int(displayed_price) == (int(total + int(tip)) if discount is False else int(int(total * 0.9) + int(tip))):
+            if tip != 0:
+                prime += int(int(tip) * 0.75)
+            return {"value": True, "prime": prime}
+        else:
+            return {"value": False, "prime": 0}
     except:
         print("Found a bill with invalid declaration, skipping...")
-        return {"value": False, "prime": 0}
-
-    if int(displayed_price) == (int(total + int(tip)) if discount is False else int(int(total * 0.9) + int(tip))):
-        if tip != 0:
-            prime += int(int(tip) * 0.75)
-        return {"value": True, "prime": prime}
-    else:
         return {"value": False, "prime": 0}
